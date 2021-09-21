bb27c7e09f64f0968b9943a17c121a29e04b573a
Maame Konadu Assasie-Gymah <maame@maames-mbp.myfiosgateway.com>
2021-09-20 20:52:14 -0400

    nav component added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 function About() {
   return (
     <section className="my-5">
+      <h1 id="about">Who am I?</h1>
       <div className="my-2">
         <div className="profile-img my-5">
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
